d70757d22f45cdd423f7dc8591a01f00b6b01865
arundati-huragi <arundatikhuragi@gmail.com>
2025-04-09 13:56:35 +0530

    my add java file

---
+++ Addition.java
@@ -0,0 +1,14 @@
+package javadevop;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        System.out.println("the sum is:"+sum);
+    }
+
+}
+
+
+
